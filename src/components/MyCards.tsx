daf3851395b8f792b981633fad5995fa700a1f5a
Wai Yan Phyo <oyhpnayiaw@gmail.com>
2025-02-28 03:12:43 +0700

    Create reuseable balance card

--- src/components/MyCards.tsx
+++ src/components/MyCards.tsx
@@ -19,10 +19,6 @@ interface MyCardsProps {
 const MyCards = ({ cards }: MyCardsProps) => {
   const [activeCardIndex, setActiveCardIndex] = useState(0);
 
-  const formatCardNumber = (number: string) => {
-    return number.match(/.{1,4}/g)?.join(" ") || number;
-  };
-
   const nextCardIndex = (activeCardIndex + 1) % cards.length;
 
   return (
@@ -38,73 +34,11 @@ const MyCards = ({ cards }: MyCardsProps) => {
         className="relative mb-2 aspect-video flex-1 cursor-pointer sm:mb-4"
         onClick={() => setActiveCardIndex(nextCardIndex)}
       >
-        <div
-          className={classNames(
-            "absolute top-[24px] left-[16px] z-10 flex aspect-video w-[calc(100%-16px)] flex-col justify-between justify-end rounded-xl p-2 opacity-90 sm:top-[30px] sm:left-[20px] sm:w-[calc(100%-20px)] sm:p-3",
-            cards[nextCardIndex].color,
-          )}
-        >
-          <div className="text-white">
-            <div className="mb-1 text-xs sm:mb-2 sm:text-base">
-              {formatCardNumber(cards[activeCardIndex].number)}
-            </div>
-            <div className="flex items-center justify-between">
-              <div className="text-[10px] sm:text-xs">
-                {cards[nextCardIndex].cardholderName}
-              </div>
-              <div className="text-[10px] sm:text-xs">
-                {cards[nextCardIndex].expiryDate}
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div
-          className={classNames(
-            "absolute flex aspect-video w-[calc(100%-24px)] flex-col justify-between rounded-xl p-3 sm:w-[calc(100%-30px)] sm:p-5",
-            "shadow-lg transition-all duration-300",
-            cards[activeCardIndex].color,
-          )}
-          style={{
-            zIndex: 20,
-          }}
-        >
-          <div className="flex items-start justify-between">
-            <div>
-              <div className="text-xs text-white sm:text-sm">Balance</div>
-              <div className="mb-1 text-sm font-semibold text-white sm:mb-2 sm:text-lg">
-                $
-                {cards[activeCardIndex].balance.toLocaleString("en-US", {
-                  minimumFractionDigits: 2,
-                })}
-              </div>
-            </div>
-            <div className="text-base font-bold text-white sm:text-xl">
-              {cards[activeCardIndex].type === "visa" && "VISA"}
-              {cards[activeCardIndex].type === "mastercard" && (
-                <img
-                  src="/mastercard-logo.png"
-                  alt="Mastercard"
-                  className="h-5 w-8 object-contain sm:h-7 sm:w-12"
-                />
-              )}
-            </div>
-          </div>
-
-          <div className="text-white">
-            <div className="line-clamp-1 text-xs sm:text-sm">
-              {formatCardNumber(cards[activeCardIndex].number)}
-            </div>
-            <div className="flex items-center justify-between">
-              <div className="text-[10px] sm:text-xs">
-                {cards[activeCardIndex].cardholderName}
-              </div>
-              <div className="text-[10px] sm:text-xs">
-                {cards[activeCardIndex].expiryDate}
-              </div>
-            </div>
-          </div>
-        </div>
+        <CardDisplay
+          cardInfo={cards[nextCardIndex]}
+          className="top-[30px] left-[16px]"
+        />
+        <CardDisplay cardInfo={cards[activeCardIndex]} className="z-20" />
       </div>
 
       <div className="mt-6 flex justify-center sm:mt-3">
@@ -119,3 +53,59 @@ const MyCards = ({ cards }: MyCardsProps) => {
 };
 
 export default MyCards;
+
+interface CardDisplayProps {
+  cardInfo: Card;
+  className?: string;
+}
+
+function CardDisplay({ cardInfo, className }: CardDisplayProps) {
+  const formatCardNumber = (number: string) => {
+    return number.match(/.{1,4}/g)?.join(" ") || number;
+  };
+
+  return (
+    <div
+      className={classNames(
+        "absolute flex aspect-video w-[calc(100%-24px)] flex-col justify-between rounded-xl p-3 sm:w-[calc(100%-30px)] sm:p-5",
+        "shadow-lg transition-all duration-300",
+        cardInfo.color,
+        className,
+      )}
+    >
+      <div className="flex items-start justify-between">
+        <div>
+          <div className="text-xs text-white sm:text-sm">Balance</div>
+          <div className="mb-1 text-sm font-semibold text-white sm:mb-2 sm:text-lg">
+            $
+            {cardInfo.balance.toLocaleString("en-US", {
+              minimumFractionDigits: 2,
+            })}
+          </div>
+        </div>
+        <div className="text-base font-bold text-white sm:text-xl">
+          {cardInfo.type === "visa" && "VISA"}
+          {cardInfo.type === "mastercard" && (
+            <img
+              src="/mastercard-logo.png"
+              alt="Mastercard"
+              className="h-5 w-8 object-contain sm:h-7 sm:w-12"
+            />
+          )}
+        </div>
+      </div>
+
+      <div className="text-white">
+        <div className="line-clamp-1 text-xs sm:text-sm">
+          {formatCardNumber(cardInfo.number)}
+        </div>
+        <div className="flex items-center justify-between">
+          <div className="text-[10px] sm:text-xs">
+            {cardInfo.cardholderName}
+          </div>
+          <div className="text-[10px] sm:text-xs">{cardInfo.expiryDate}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
